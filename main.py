cab39571eb9d786ad9c74b4b26d9afa0eaeabee4
celestialxevermore <key2317@naver.com>
2023-03-09 23:36:27 +0900

    initializing files

---
+++ main.py
@@ -0,0 +1,54 @@
+import os 
+import torch 
+import random 
+import numpy as np 
+from config.configuration import configuration
+from torch.utils.tensorboard.writer import SummaryWriter
+from modules.tokenization_clip import SimpleTokenizer as ClipTokenizer
+from dataloaders.data_factory import DataDirector 
+from utils.utilities import parallel_apply, get_logger, prep_optimizer
+from utils.setting import Initializer
+
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]="5"
+torch.distributed.init_process_group(backend="nccl")
+
+def main():
+    global logger 
+    configuration = configuration()
+    initializer = Initializer()
+    if not configuration.no_tensorboard:
+        writer = SummaryWriter(log_dir = configuration.tb_log_dir)
+    else:
+        writer = None 
+    
+    tokenizer = ClipTokenizer()
+    configuration,logger = initializer.set_seed_logger(configuration)
+    device,n_gpu = initializer.init_device(configuration,configuration.local_rank)
+    
+    model = initializer.init_model(configuration, device, n_gpu, configuration.local_rank)
+    
+    
+    ## ####################################
+    # train and eval
+    ## ####################################
+    
+    if configuration.do_train:
+        train_dataloader,train_length,train_sampler = DataDirector.get_dataloader(configuration,tokenizer,split_type=configuration.do_train)
+        num_train_optimization_steps = (int(len(train_dataloader) + configuration.gradient_accumulation_steps - 1)
+                                        / configuration.gradient_accumulation_steps) * configuration.epochs
+
+        optimizer,scheduler,model = initializer.prep_optimizer(configuration,model,num_train_optimization_steps,device,n_gpu,configuration.local_rank,coef_lr=configuration.coef_lr)
+        if configuration.local_rank == 0:
+            logger.info("***** Running training *****")
+            logger.info("  Num examples = %d", train_length)
+            logger.info("  Batch size = %d", configuration.batch_size)
+            logger.info("  Num steps = %d", num_train_optimization_steps * configuration.gradient_accumulation_steps)
+
+        #if configuration.resume_model:
+        
+    
+    elif configuration.do_eval:
+        test_Dataloader,test_length = DataDirector.get_dataloader(configuration,tokenizer,configuration.do_eval)
+
+        
---
+++ utils/setting.py
@@ -0,0 +1,109 @@
+import os 
+import torch 
+import numpy as np 
+import random
+from utilities import parallel_apply,get_logger
+from modules.optimization import BertAdam
+
+class Initializer():
+    def __init__(self):
+        super(Initializer,self).__init__()
+        
+    def set_seed_logger(self,configuration):
+        # predefining random initial seeds
+        random.seed(configuration.seed)
+        os.environ['PYTHONHASHSEED'] = str(configuration.seed)
+        np.random.seed(configuration.seed)
+        torch.manual_seed(configuration.seed)
+        torch.cuda.manual_seed(configuration.seed)
+        torch.cuda.manual_seed_all(configuration.seed)  # if you are using multi-GPU.
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
+
+        world_size = torch.distributed.get_world_size()
+        torch.cuda.set_device(configuration.local_rank)
+        configuration.world_size = world_size
+        rank = torch.distributed.get_rank()
+        configuration.rank = rank
+
+        if not os.path.exists(configuration.output_dir):
+            os.makedirs(configuration.output_dir, exist_ok=True)
+
+        logger = get_logger(os.path.join(configuration.output_dir, "log.txt"))
+
+        if configuration.local_rank == 0:
+            logger.info("Effective parameters:")
+            for key in sorted(configuration.__dict__):
+                logger.info("  <<< {}: {}".format(key, configuration.__dict__[key]))
+
+        return configuration,logger
+
+    def init_device(self,configuration, local_rank):
+        global logger
+
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu", local_rank)
+
+        n_gpu = torch.cuda.device_count()
+        logger.info("device: {} n_gpu: {}".format(device, n_gpu))
+        configuration.n_gpu = n_gpu
+
+        if configuration.batch_size % configuration.n_gpu != 0 or configuration.batch_size_val % configuration.n_gpu != 0:
+            raise ValueError("Invalid batch_size/batch_size_val and n_gpu parameter: {}%{} and {}%{}, should be == 0".format(
+                configuration.batch_size, configuration.n_gpu, configuration.batch_size_val, configuration.n_gpu))
+
+        return device, n_gpu
+
+    def init_model(self,configuration, device, n_gpu, local_rank):
+
+        if configuration.init_model:
+            model_state_dict = torch.load(configuration.init_model, map_location='cpu')
+        else:
+            model_state_dict = None
+
+        # Prepare model
+        cache_dir = configuration.cache_dir if configuration.cache_dir else os.path.join(str(PYTORCH_PRETRAINED_BERT_CACHE), 'distributed')
+        model = XVLNet.from_pretrained(configuration.cross_model, cache_dir=cache_dir, state_dict=model_state_dict, task_config=configuration)
+
+        model.to(device)
+
+        return model
+
+    def prep_optimizer(self,configuration, model, num_train_optimization_steps, device, n_gpu, local_rank, coef_lr=1.):
+
+        if hasattr(model, 'module'):
+            model = model.module
+
+        param_optimizer = list(model.named_parameters())
+        no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+
+        decay_param_tp = [(n, p) for n, p in param_optimizer if not any(nd in n for nd in no_decay)]
+        no_decay_param_tp = [(n, p) for n, p in param_optimizer if any(nd in n for nd in no_decay)]
+
+        decay_clip_param_tp = [(n, p) for n, p in decay_param_tp if "clip." in n]
+        decay_noclip_param_tp = [(n, p) for n, p in decay_param_tp if "clip." not in n]
+
+        no_decay_clip_param_tp = [(n, p) for n, p in no_decay_param_tp if "clip." in n]
+        no_decay_noclip_param_tp = [(n, p) for n, p in no_decay_param_tp if "clip." not in n]
+
+        weight_decay = 0.2
+        optimizer_grouped_parameters = [
+            {'params': [p for n, p in decay_clip_param_tp], 'weight_decay': weight_decay, 'lr': configuration.lr * coef_lr},
+            {'params': [p for n, p in decay_noclip_param_tp], 'weight_decay': weight_decay},
+            {'params': [p for n, p in no_decay_clip_param_tp], 'weight_decay': 0.0, 'lr': configuration.lr * coef_lr},
+            {'params': [p for n, p in no_decay_noclip_param_tp], 'weight_decay': 0.0}
+        ]
+
+        self.scheduler = None
+        optimizer = BertAdam(optimizer_grouped_parameters, lr=configuration.lr, warmup=configuration.warmup_proportion,
+                            schedule='warmup_cosine', b1=0.9, b2=0.98, e=1e-6,
+                            t_total=num_train_optimization_steps, weight_decay=weight_decay,
+                            max_grad_norm=1.0)
+
+        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[local_rank],
+                                                        output_device=local_rank, find_unused_parameters=True)
+
+        return optimizer, self.scheduler, model
+
+
+
+    
