18c8e20bd29d6c9cc8c66e5ef945f377440ee56a
celestialxevermore <key2317@naver.com>
2023-03-15 23:51:11 +0900

    main

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import os
 import torch 
 import random 
 import numpy as np 
-from config.configuration import configuration
+from config.configuration import Configuration
 from torch.utils.tensorboard.writer import SummaryWriter
 from modules.tokenization_clip import SimpleTokenizer as ClipTokenizer
 from dataloaders.data_factory import DataDirector 
 from utils.utilities import parallel_apply, get_logger, prep_optimizer
 from utils.setting import Initializer
+from utils.train_test import Trainer,Tester
 
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"]="5"
@@ -15,7 +16,7 @@ torch.distributed.init_process_group(backend="nccl")
 
 def main():
     global logger 
-    configuration = configuration()
+    configuration = Configuration()
     initializer = Initializer()
     if not configuration.no_tensorboard:
         writer = SummaryWriter(log_dir = configuration.tb_log_dir)
@@ -24,7 +25,7 @@ def main():
     
     tokenizer = ClipTokenizer()
     configuration,logger = initializer.set_seed_logger(configuration)
-    device,n_gpu = initializer.init_device(configuration,configuration.local_rank)
+    device,n_gpu = initializer.init_device(configuration,logger,configuration.local_rank)
     
     model = initializer.init_model(configuration, device, n_gpu, configuration.local_rank)
     
@@ -46,6 +47,7 @@ def main():
             logger.info("  Num steps = %d", num_train_optimization_steps * configuration.gradient_accumulation_steps)
 
         #if configuration.resume_model:
+        trainer = Trainer(model,optimizer,configuration,train_dataloader,scheduler,device,train_sampler,logger)
         
     
     elif configuration.do_eval:
